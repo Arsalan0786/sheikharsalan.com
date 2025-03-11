45c65878afaf0186a35725710d5e7cf9d428b7a2
Sheikh Arsalan <125729616+Arsalan0786@users.noreply.github.com>
2025-03-12 04:38:27 +0530

    Update index.js

--- Portfolio2/src/index.js
+++ Portfolio2/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/App.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: Home(),
+  },
+  {
+    path: "/test",
+    element: Test(),
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
